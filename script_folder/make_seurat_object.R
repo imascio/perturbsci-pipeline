45cb11aaf2a7496e36333d79ab9c664b60bea986
Isabella Mascio <itm220@nyu.edu>
2023-09-25 11:53:15 -0400

    add gdo to R script

--- script_folder/make_seurat_object.R
+++ script_folder/make_seurat_object.R
@@ -15,7 +15,7 @@ library(genio)
 
 
 ## read in arguments following terminal command
-# order of arguments: sampleID.txt input_dir_counts_matrix output_dir output_name.rds
+# order of arguments 1)gex_sampleID.txt 2)input_dir_counts_matrix 3)output_dir 4)output_name.rds 5)gdo_processing_dir
 args <- commandArgs(trailingOnly = TRUE)
 
 ############################GEX PROCESSING####################################################
@@ -66,4 +66,102 @@ rt.barcodes <- substr( rtlig.barcodes , start = 11 , stop = 20)
 objv5$rt.barcode <- rt.barcodes
 objv5$lig.barcode <- ligation.barcodes
 
-saveRDS(objv5,paste0(args[3],"/",args[4]))
+# save seurat object that is only gex counts right now
+saveRDS(objv5,paste0(args[3],"/gex_processing.rds"))
+##############################################################################################
+
+############################GDO PROCESSING####################################################
+# prepping guide matrix  - Zihan from Cao Lab gRNA_MM_formatting.R
+sciRNAseq_gene_count_summary <- function (gene_count_folder) {
+  gene_matrix_dir = paste(gene_count_folder, "/gRNA_mat.count", sep = "")
+  df_gene_dir = paste(gene_count_folder, "/gRNA_annotat.report", sep = "")
+  df_cell_dir = paste(gene_count_folder, "/cell_gRNA_annotat.report", sep = "")
+  
+  df_gene = read.csv(df_gene_dir, header = F)
+  df_cell = read.csv(df_cell_dir, header = F)
+  gene_matrix = read.csv(gene_matrix_dir, header = F)
+  colnames(df_gene) = c("gRNA_index", "gRNA_name")
+  colnames(df_cell) = c("cell_index", "cell_name")
+  colnames(gene_matrix) = c("gRNA_index", "cell_index", "count")
+  rownames(df_gene) = df_gene$gene_id
+  rownames(df_cell) = df_cell$cell_name
+  gene_count = sparseMatrix(i = gene_matrix$gRNA_index, j = gene_matrix$cell_index, x = gene_matrix$count)
+  df_gene = df_gene[1:nrow(gene_count), ]
+  rownames(gene_count) = df_gene$gRNA_name
+  colnames(gene_count) = df_cell$cell_name
+  
+  return(list(df_cell, df_gene, gene_count))
+}
+
+# save location of report folder
+report_folder = paste0(args[5],"/gRNA_report")
+
+# format the output into a sparse matrix ready for R analysis
+result = sciRNAseq_gene_count_summary(report_folder)
+gRNA_df_cell = result[[1]]
+gRNA_df_gene = result[[2]]
+gRNA_count = result[[3]]
+# saving guide data as RData
+save(gRNA_df_cell, gRNA_df_gene, gRNA_count, file = paste0(args[3], "/gRNA_Summary.RData"))
+
+#saving sparse matrix to matrix, duplicating the single row and making new row names
+gRNA_mat <- as.matrix(gRNA_count)
+colnames_gdo <- colnames(gRNA_mat)
+# making a table of the combinations of barcodes in order of colnames
+gdo_bc_combinations <- data.frame(inneri7 = substr(gsub("\\..*","",colnames_gdo) , start = 4 , stop = 13),
+                                  shortdT = substr(gsub(".*\\.","",colnames_gdo) , start = 11 , stop = 20),
+                                  lig = substr(gsub(".*\\.","",colnames_gdo) , start = 1 , stop = 10))
+
+# making inner i7 list
+i7list <- data.frame(i7_ID = as.character(c(1:8)),
+                     inneri7 = c("TCGGATTCGG", "CTAAGCCTTG", "CTAACTAGGT", "GCAAGACCGT", "ATGGAACGAA", "TAGAGGCGTT", "GCATCGTATG", "TGGACGACTA"))
+gdo_bc_i7ID <- left_join(x = gdo_bc_combinations, y = i7list)
+new_gd_colnames <- paste0(gdo_bc_i7ID$i7_ID, "_" ,gdo_bc_i7ID$lig,gdo_bc_i7ID$shortdT)
+colnames(gRNA_mat) <- new_gd_colnames
+# save processed guide matrix
+saveRDS(gRNA_mat, paste0(args[3],"gdo_procesed_matrix.rds"))
+#################################################################################################
+
+##############################MERGING GEX AND GDO INTO ONE OBJECT################################
+### find cells (col names) that intersect with gex and gdo & make new gdo matrix with those
+cell.intersect <- intersect(colnames(objv5), colnames(gRNA_mat))
+
+### find cells (col names) that are in gex but missing in gdo
+missing_cells <- colnames(objv5)[!(colnames(objv5) %in% colnames(gRNA_mat))]
+
+### make matrix with 0 for gdo counts of missing cells then cbind it to intersected gdo matrix
+missing_guides <- matrix(data = 0, nrow = nrow(gRNA_mat), ncol = length(missing_cells))
+colnames(missing_guides) <- missing_cells
+rownames(missing_guides) <- rownames(gRNA_mat)
+
+gdo.mat <- cbind(gRNA_mat, missing_guides)
+
+# subset gdo.mat to only have cells in gex obj
+all(colnames(objv5) %in% colnames(gdo.mat))
+
+gdo.subset <- gdo.mat[,colnames(objv5)]
+
+### merge gex seurat objects and add gdo assay to it
+obj <- objv5
+obj[["GDO"]] <- CreateAssayObject(counts = gdo.subset)
+#################################################################################################
+
+##############################ADD GDO META#############################################
+## adding guide metadata
+# making dataframe with cell names, guide counts, and a binning category
+guide.meta <- data.frame(cells = names(obj$nCount_GDO), counts = obj$nCount_GDO, guide_group = NA)
+# guides less than 10
+guide.meta[guide.meta$counts < 10, 3] <- "<10"
+# guides 10 to less than 50
+guide.meta[guide.meta$counts >= 10 & guide.meta$counts < 50, 3] <- "10-50"
+# guides 50 to less than 100
+guide.meta[guide.meta$counts >= 50 & guide.meta$counts < 100, 3] <- "50-100"
+# guides 100 or more
+guide.meta[guide.meta$counts >= 100, 3] <- "100+"
+
+
+guide.meta2 <- data.frame(sgRNA_count = guide.meta$guide_group, row.names = guide.meta$cells)
+
+obj <- AddMetaData(obj, metadata = guide.meta2)
+saveRDS(obj, paste0(args[3],"/",args[4]))
+#######################################################################################

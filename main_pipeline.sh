166663acc677d55ce3d6af0c855247420a09dfea
Isabella Mascio <93402278+imascio@users.noreply.github.com>
2023-10-26 13:46:04 -0400

    Update main_pipeline.sh

--- main_pipeline.sh
+++ main_pipeline.sh
@@ -2,15 +2,12 @@
 
 #run as bash main_pipeline.sh > output.log 2> messages.log
 
-
 ## Location of the bashrc file to activate the conda environments.
-#bashrc_location="/home/dalgarnoc/.bashrc"
-#activate_location="/home/dalgarnoc/miniconda3/bin/activate"
-#source $activate_location
+activate_location="~/.bashrc"
+source $activate_location
 conda activate EasySci
 
 # Define script directory
-
 script_path="/brahms/shared/EasySci/shell_pipeline/script_folder"
 
 ###################### DEFINE INPUT VARIABLES ######################
@@ -77,7 +74,6 @@ gRNA_annotation_df=$project_folder/APA_1SRSF7_sgRNA_info_table.txt
 # define the name of the final seuart object - should be .rds extension
 seurat_object_name=rna1_seurat_obj.rds
 
-
 ############ BARCODE EXTRACTION ############
 
 # the script take an input folder, a sample ID list, an output folder, the RT barcode list, the ligation barcode list and core number and a rendom hexamer barcode list. Then it extract the RT barcode from read1, the ligation barocde from read2, correct them to the nearest RT and ligation barcode (with edit distance <= 1), and attach the RT and ligation barcode and UMI sequence to the read name of read1 and read3. Reads with unmatched RT or ligation barcodes are discarded.
@@ -90,12 +86,16 @@ now=$(date +"%T")
 echo "Changing the name of the gex fastq files... $now" >&2
 
 echo $sample_ID >&2
+
 for sample in $(cat $sample_ID); do echo changing name $sample; mv $input_folder/*$sample*R1*.fastq.gz $input_folder/$sample.R1.fastq.gz; mv $input_folder/*$sample*R2*.fastq.gz $input_folder/$sample.R2.fastq.gz; mv $input_folder/*$sample*R3*.fastq.gz $input_folder/$sample.R3.fastq.gz; done
 
 now=$(date +"%T")
 echo "Attaching barcode and UMI.... $now" >&2
+
 mkdir -p $output_folder
+
 python $script $input_folder $sample_ID $output_folder $ligation_barcode $RT_barcode $core $randomN_barcode_file
+
 now=$(date +"%T")
 echo "Barcode transformed and UMI attached. $now" >&2
 
@@ -115,6 +115,7 @@ parallel -j ${N_JOBS} --verbose bash $script_path/trimming.sh $input_folder {} $
 ################# REMOVE EMPTY LINES #################
 
 # This step removes any empty fastq lines resulting from trimming reads that are entirely poly A
+
 now=$(date +"%T")
 echo "Removing empty lines from fastqs... $now" >&2
 
@@ -128,6 +129,7 @@ now=$(date +"%T")
 echo "Empty lines removed. $now" >&2
 
 ############ ALIGN ############
+
 now=$(date +"%T")
 echo "Aligning cleaned fastq files... $now" >&2
 
@@ -143,6 +145,7 @@ echo "Alignment complete. $now" >&2
 ############ FEATURE ############
 
 # This step maps aligned reads to genes using the umi tools feature function
+
 now=$(date +"%T")
 echo "Mapping reads to genes... $now" >&2
 
@@ -158,6 +161,7 @@ echo "Mapping to genes complete. $now" >&2
 ############ COUNT ############
 
 # Remove UMI duplicates and generate a cell x gene counts matrix
+
 now=$(date +"%T")
 echo "Generating a counts matrix! Almost done w gex processing... $now" >&2
 
@@ -173,19 +177,24 @@ parallel -j ${N_JOBS} --verbose bash $script_path/count.sh $input_folder $output
 mkdir -p $gdo_processing_folder
 
 # Change the file names of raw gdo fastq.gz
+
 now=$(date +"%T")
 echo "Changing the name of the gdo fastq files... $now" >&2
+
 for sample in $(cat $gdo_sample_ID); do echo changing name $sample; mv $fastq_folder/*$sample*R1_001.fastq.gz $fastq_folder/$sample.R1.fastq.gz; mv $fastq_folder/*$sample*R2_001.fastq.gz $fastq_folder/$sample.R2.fastq.gz; mv $fastq_folder/*$sample*R3_001.fastq.gz $fastq_folder/$sample.R3.fastq.gz; done
 
 # Run the guide counting script
+
 now=$(date +"%T")
 echo "Processing the gdo reads into single-cell counts matrix (must be reformatted for seurat later)... $now" >&2
+
 python3 $script_path/sgrna_count.py $fastq_folder ${gdo_sample_ID} $gdo_processing_folder $RT_barcode $inner_i7_bc_file $ligation_barcode $gRNA_correction_file $gRNA_annotation_df $cutoff $core
 
 
 ############ SEURAT ############
 
 # Make a seurat object from the counts matrices
+
 now=$(date +"%T")
 echo "Generating a seurat object! Last step... $now" >&2
 
@@ -197,4 +206,3 @@ Rscript $script_path/make_seurat_object.R $sample_ID $input_folder $output_folde
 
 now=$(date +"%T")
 echo "You're done ;) $now" >&2
-

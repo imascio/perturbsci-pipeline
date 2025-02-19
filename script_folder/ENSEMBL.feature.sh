3b94054bcd8c2f919220a2b21afcdd8670ac4468
Isabella Mascio <93402278+imascio@users.noreply.github.com>
2025-02-19 14:59:33 -0500

    Update ENSEMBL.feature.sh

--- script_folder/ENSEMBL.feature.sh
+++ script_folder/ENSEMBL.feature.sh
@@ -10,7 +10,7 @@ output_folder=$4
 
 
 featureCounts -a $gtf_file \
-              -o ${output_folder}/gene_assigned \
+              -o ${output_folder}/${sampleID}_gene_assigned \
               -R BAM ${input_folder}/${sampleID}Aligned.sortedByCoord.out.bam \
               -T 4;            
 samtools sort ${output_folder}/${sampleID}Aligned.sortedByCoord.out.bam.featureCounts.bam -o ${output_folder}/${sampleID}_assigned_sorted.bam;

93cc06df6abdb5684e5aebdcd50bf33e9bd180ae
Isabella Mascio <93402278+imascio@users.noreply.github.com>
2025-02-26 17:19:44 -0500

    Update count.sh

--- script_folder/count.sh
+++ script_folder/count.sh
@@ -9,5 +9,5 @@ input_folder=$1
 output_folder=$2
 sampleID=$3
 
-umi_tools count --per-gene --gene-tag=XT --assigned-status-tag=XS --per-cell -I ${input_folder}/${sampleID}_assigned_sorted.bam -S ${output_folder}/${sampleID}_counts.tsv.gz --wide-format-cell-counts
+umi_tools count --per-gene --gene-tag=XT --assigned-status-tag=XS --skip-tags-regex="Unassigned" --per-cell -I ${input_folder}/${sampleID}_assigned_sorted.bam -S ${output_folder}/${sampleID}_counts.tsv.gz --wide-format-cell-counts
 
